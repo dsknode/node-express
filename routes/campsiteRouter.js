6a740b7f604137f523b1dff3d7c4eb3d3b63d1d6
Darrin <dsknode@gmail.com>
2020-03-14 23:34:33 -0500

    workshop

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -25,4 +25,25 @@ campsiteRouter.route('/')
   res.end('deleting all campsites');
 });
 
+campsiteRouter.route('/:campsiteId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain'); // why plain text?
+    next();
+})
+.get((req, res) => {
+    res.end(`will send details of the campsite: ${req.params.campsiteId} to you`);
+})
+.post((req, res) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /campsites');
+})
+.put((req, res) => {
+    res.write(`updating the campsite: ${req.params.campsiteId}\n`);
+    res.end(`Will update the campsite:${req.body.name} with the description:${req.body.description}`);
+})
+.delete((req, res) => {
+    res.end(`deleting campsite: ${req.params.campsiteId}`);
+});
+
 module.exports = campsiteRouter;
